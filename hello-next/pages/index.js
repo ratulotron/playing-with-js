107ae16428068625428993a3f46030b9fcd89482
Ratul Minhaz <m@ratul.xyz>
2017-06-03 03:06:29 +0600

    Tested using components

---
+++ hello-next/pages/index.js
@@ -0,0 +1,24 @@
+// import Link from 'next/link'
+// import Header from '../components/Header'
+
+// pages/index.js
+
+import Layout from '../components/MyLayout.js'
+
+export default () => (
+    <Layout>
+       <p>Hello Next.js</p>
+    </Layout>
+)
+
+// const Index = () => (
+//   <div>
+//     {/*<Link href="/about">
+//       <a>About Page</a>
+//     </Link>*/}
+//     <Header />
+//     <p>Hello Next.js</p>
+//   </div>
+// )
+
+// export default Index
