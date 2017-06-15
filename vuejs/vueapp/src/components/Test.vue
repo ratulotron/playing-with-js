94d62ff40b5f2c1b44ed548cbf54979247b960f6
ratulotron <m@ratul.xyz>
2017-06-15 16:39:59 +0600

    Temp commit

---
+++ vuejs/vueapp/src/components/Test.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="test">
+        <h1>{{title}}</h1>
+        <p v-if="showName">{{user.firstName}}</p>
+        <p v-else>Nobody</p>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: 'test',
+    data() {
+        return {
+            title: 'Hello World',
+            user: {
+                firstName: 'John',
+                lastName: 'Doe',
+            },
+            showName:false
+        }
+    }
+}
+</script>
+
+
+<style scoped>
+
+</style>
