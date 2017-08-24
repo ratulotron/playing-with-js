a80b55602aa578ad692344a8892a20484fc7ad93
ratulotron <m@ratul.xyz>
2017-08-24 17:52:32 +0600

    Started elm

---
+++ elm/main.elm
@@ -0,0 +1,21 @@
+module Main exposing (..)
+
+import Html exposing (text)   
+
+politely : String -> String
+politely phrase = 
+    "Excuse me, " ++ phrase
+
+ask : String -> String -> String 
+ask thing place = 
+    "is there a " ++ thing ++ " in the " ++ place ++ "?"
+
+askPolitelyAboutFish : String -> String
+askPolitelyAboutFish = 
+    politely << (ask "fish")
+
+main =
+    -- text 
+    -- <| politely
+    -- <| ask "fish" "Uttara"
+    text <| askPolitelyAboutFish "hat"
