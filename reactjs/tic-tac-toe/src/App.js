8f74b9c39bb643fb62c111344bcef6ddfc55d9bd
ratulotron <m@ratul.xyz>
2018-01-28 12:10:24 +0600

    Declaring a Winner

--- reactjs/tic-tac-toe/src/App.js
+++ reactjs/tic-tac-toe/src/App.js
@@ -23,6 +23,11 @@ class Board extends Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice();
+    // Just when someone completed their move, check whether they have won
+    // or not
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
       squares: squares,
@@ -41,7 +46,13 @@ class Board extends Component {
   }
 
   render () {
-    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
 
     return (
       <div className="App">
@@ -82,4 +93,29 @@ class Game extends Component {
     );
   }
 }
+
+function calculateWinner(squares) {
+  // Winning combinations
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    // Take one combination
+    const [a, b, c] = lines[i];
+    // Check whether this combination exists in `squares`
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
 export default Game;
